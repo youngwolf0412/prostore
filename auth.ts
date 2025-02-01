93ade636b937479bc9d06ca3ac6c1de686e53e74
Brijesh Yadav <kumar.brijesh0412@gmail.com>
2025-02-01 12:59:30 +0530

    auth ts file protected route updated

--- auth.ts
+++ auth.ts
@@ -116,32 +116,14 @@ export const config = {
       return token;
     },
     authorized({ request, auth }: any) {
-      // Array of regex patterns of paths we want to protect
-      const protectedPaths = [
-        /\/shipping-address/,
-        /\/payment-method/,
-        /\/place-order/,
-        /\/profile/,
-        /\/user\/(.*)/,
-        /\/order\/(.*)/,
-        /\/admin/,
-      ];
-
-      // Get pathname from the req URL object
-      const { pathname } = request.nextUrl;
-
-      // Check if user is not authenticated and accessing a protected path
-      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
-
-      // Check for session cart cookie
+      // check for session cart cookie
       if (!request.cookies.get("sessionCartId")) {
         // Generate new session cart id cookie
         const sessionCartId = crypto.randomUUID();
 
-        // Clone the req headers
+        //  Clone the req headers
         const newRequestHeaders = new Headers(request.headers);
-
-        // Create new response and add the new headers
+        // create new response and add the new headers
         const response = NextResponse.next({
           request: {
             headers: newRequestHeaders,
@@ -150,7 +132,6 @@ export const config = {
 
         // Set newly generated sessionCartId in the response cookies
         response.cookies.set("sessionCartId", sessionCartId);
-
         return response;
       } else {
         return true;
